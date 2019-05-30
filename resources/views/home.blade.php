005861ee6ec5c33fd4ab9391134f00d04ad7bce0
Laciann <abhishek.acharya@apexcollege.edu.np>
2019-05-30 17:51:28 +0545

    This is a new commit for what I originally planned to be amended

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <img src="https://instagram.fktm10-1.fna.fbcdn.net/vp/6244e453068ff9fac39d014166c59266/5D79BFC8/t51.2885-19/s320x320/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fktm10-1.fna.fbcdn.net" alt="fcc" class="rounded-circle p-5" style="height:200px">
+            </div>
+            <div class="col-9 pt-5 pl-5">
+            <div><h1>FreeCodeCamp</h1></div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>100</strong> posts</div>
+                    <div class="pr-5"><strong>23k</strong> followers</div>
+                    <div class="pr-5"><strong>212</strong> following</div>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+@endsection
