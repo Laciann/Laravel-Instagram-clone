21e645a8d7c76b6193f6b7b26a39856ec7600368
Laciann <abhishek.acharya@apexcollege.edu.np>
2019-05-30 18:20:13 +0545

    frontend done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,10 +13,16 @@
                     <div class="pr-5"><strong>23k</strong> followers</div>
                     <div class="pr-5"><strong>212</strong> following</div>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>freeCodeCamp.org</div>
+                <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+                <div><a href="http://www.freecodecamp.org">www.freecodecamp.org</a></div>
             </div>
+        </div>
+
+        <div class="row pt-5">
+            <div class="col-4"><img src="https://instagram.fktm10-1.fna.fbcdn.net/vp/8341976c51696518d91674dfd62c33fa/5D89CDBD/t51.2885-15/sh0.08/e35/c0.104.921.921/s640x640/59781813_2308953272503125_6520484961724886162_n.jpg?_nc_ht=instagram.fktm10-1.fna.fbcdn.net" alt="baby" class="w-100"></div>
+            <div class="col-4"><img src="https://instagram.fktm10-1.fna.fbcdn.net/vp/fe62f2b611baf86f6a24032251ab6e34/5D7E2766/t51.2885-15/sh0.08/e35/c1.0.747.747/s640x640/60581454_869432733405043_458079974647968282_n.jpg?_nc_ht=instagram.fktm10-1.fna.fbcdn.net" alt="dog" class="w-100"></div>
+            <div class="col-4"><img src="https://instagram.fktm10-1.fna.fbcdn.net/vp/4eed540aa5f7034f12ae07c1d4d37f8a/5D95524C/t51.2885-15/sh0.08/e35/c0.116.927.927a/s640x640/52557170_264243431175431_2811265639128536873_n.jpg?_nc_ht=instagram.fktm10-1.fna.fbcdn.net" alt="girl" class="w-100"></div>
         </div>
     </div>
 @endsection
